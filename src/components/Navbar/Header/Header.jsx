cd66fb9f67235a063782d0178a52977cbd3413a6
PranithaHansika <pranithahansika.kataram@gmail.com>
2025-07-23 22:18:02 +0530

    Initial commit

---
+++ src/components/Navbar/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order Your Favourite Food here</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum quidem voluptate, eum velit, maiores beatae dicta mollitia repudiandae, quia magnam quam vero et deserunt? Odio atque aspernatur a dolores in.</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
