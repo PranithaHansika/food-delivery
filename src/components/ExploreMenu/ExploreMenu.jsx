b73544553e468d6d83caa38a6d29e4c716b48506
PranithaHansika <pranithahansika.kataram@gmail.com>
2025-07-26 12:22:07 +0530

    Logic

--- src/components/ExploreMenu/ExploreMenu.jsx
+++ src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 import './ExploreMenu.css'
-const ExploreMenu = () => {
+import {menu_list} from '../../assets/assets'
+const ExploreMenu = ({category,setCategory}) => {
   return (
-    <div>
+    <div className='explore-menu' id='explore-menu'>
+      <h1>Explore Our Menu</h1>
+      <p className='explore-menu-text'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio cupiditate id natus officia voluptate, deleniti eaque soluta similique possimus harum ducimus quaerat adipisci maxime unde sint, corporis, illo labore in!</p>
+      <div className="explore-menu-list">
+        {menu_list.map((item,index)=>{
+          return(
+            <div onClick={()=>setCategory(prev=>prev===item.menu_name?"All":item.menu_name)} key={index} className="explore-menu-list-item">
+                <img className={category===item.menu_name?"active":""} src={item.menu_image} alt="" />
+                <p>{item.menu_name}</p>
+            </div>
+          )
+        })}
+      </div>
+      <hr />
       
     </div>
   )
